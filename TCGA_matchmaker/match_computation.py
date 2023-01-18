2b4a3aae24af0e65ea44c8159b51f0af726f9e71
Cristina Mitrea <mitrea@umich.edu>
2023-01-18 10:25:17 -0500

    Updated design docs and code

---
+++ TCGA_matchmaker/match_computation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+def read_expr_profile(file_name):
+	profile_data = pd.read_csv(file_name, sep = "\t")
+	return profile_data
+
+def read_TCGA_sample(file_name):
+	sample_data = None
+	# read sample data
+	return sample_data
+
+def test_match(profile, sample_data, threshold):
+	is_match = False
+	is_present = check_profile(profile, sample_data)
+	if is_present:
+		distance = compute_distance(profile, sample_data)
+		is_match = distance < threshold
+	return is_match
+
+def check_profile(profile, sample_data)
+	is_present = False
+	# check if all genes in the profile are present
+	return is_present
+
+def compute_distance(profile, sample_data)
+	distance = 0
+	# compute distance here
+	return distance
